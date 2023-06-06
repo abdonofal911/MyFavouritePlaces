e6d2d057d12dfb140bee20222465807607b6cbb7
Abdulrahman Nofal <abdonofa1712@gmail.com>
2023-06-06 13:18:18 +0300

    Changes to be committed: 	new file:   components/Places/PlaceForm.js 	modified:   screens/AddPlace.js 	modified:   screens/PlaceDetails.js

--- screens/AddPlace.js
+++ screens/AddPlace.js
@@ -1,10 +1,10 @@
 import React from 'react'
+import { View } from 'react-native'
+import PlaceForm from '../components/Places/PlaceForm'
 
 const AddPlace = () => {
   return (
-    <div>
-      
-    </div>
+    <PlaceForm /> 
   )
 }
 
