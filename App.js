000efb3567c5e631b3006f1012c35ea22fe70522
Abdulrahman Nofal <abdonofa1712@gmail.com>
2023-06-06 12:32:49 +0300

    Changes to be committed: 	modified:   App.js 	new file:   components/Button.js 	new file:   screens/AddPlace.js 	new file:   screens/AllPlaces.js 	new file:   screens/Map.js 	new file:   screens/PlaceDetails.js

--- App.js
+++ App.js
@@ -1,10 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import AppButton from './components/Button';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <AppButton
+      onPress={() => {
+        console.log("Button pressed!");
+      }}
+      title="Click Me!"
+    />
       <StatusBar style="auto" />
     </View>
   );
---
+++ components/Button.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import { View, Button, StyleSheet, TouchableOpacity, Text } from "react-native";
+
+const AppButton = ({ onPress, title }) => (
+  <TouchableOpacity
+    onPress={onPress}
+    style={styles.appButtonContainer}
+  >
+    <Text style={styles.appButtonText}>{title}</Text>
+  </TouchableOpacity>
+);
+
+const styles = StyleSheet.create({
+  appButtonContainer: {
+    width: 100,
+    height: 40,
+    backgroundColor: "purple",
+    borderRadius: 5,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  appButtonText: {
+    fontSize: 18,
+    color: "white",
+  },
+});
+
+export default AppButton;
