089ad652e52c5a477e5f5b496d8a2e6410ab2d2c
Abdulrahman Nofal <abdonofa1712@gmail.com>
2023-06-08 13:09:29 +0300

    Changes to be committed: 	modified:   components/Places/ImagePicker.js

--- components/Places/ImagePicker.js
+++ components/Places/ImagePicker.js
@@ -1,13 +1,40 @@
-import { Button, View } from "react-native";
-import { launchCameraAsync } from "expo-image-picker";
+import { Alert, Button, View } from "react-native";
+import {
+  launchCameraAsync,
+  useCameraPermissions,
+  PermissionStatus,
+} from "expo-image-picker";
 
 const ImagePicker = () => {
+  const [cameraPermissionInformation, requestPermission] =
+    useCameraPermissions();
+
+  async function verifyPermission() {
+    if (cameraPermissionInformation.status === PermissionStatus.UNDETERMINED) {
+      const permisssionResponse = await requestPermission();
+
+      return permisssionResponse.granted;
+    }
+    if (cameraPermissionInformation.status === PermissionStatus.DENIED) {
+      Alert.alert(
+        "Insufficient Permission!!",
+        "you need to grant camera permission to use this app"
+      );
+      return false;
+    }
+    return true;
+  }
   async function takeImageHandler() {
+    const hasPermission = await verifyPermission();
+
+    if (!hasPermission) {
+      return;
+    }
+
     const image = await launchCameraAsync({
       allowsEditing: true,
       aspect: [16, 9],
       quality: 0.5,
-      
     });
     console.log(image);
   }
