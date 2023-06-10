fcafe8b7191a6bc0742409e482c2c72456e58f10
Abdulrahman Nofal <abdonofa1712@gmail.com>
2023-06-10 13:07:04 +0300

    Changes to be committed: 	modified:   components/Places/LocatinPicker.js 	new file:   util/location.js

--- components/Places/LocatinPicker.js
+++ components/Places/LocatinPicker.js
@@ -1,4 +1,4 @@
-import { Alert, StyleSheet, View } from "react-native";
+import { Alert, Image, StyleSheet, Text, View } from "react-native";
 import OutlinedButton from "../UI/OutlinedButton";
 import { Colors } from "../../constants/colors";
 import {
@@ -6,8 +6,11 @@ import {
   useForegroundPermissions,
   PermissionStatus,
 } from "expo-location";
+import { useState } from "react";
+import { getMapPreview } from "../../util/location";
 
 function LocationPicker() {
+  const [pickedLocation, setPickedLocation] = useState();
   const [locationInformationInformation, requestPermission] =
     useForegroundPermissions();
   async function verifyPermissions() {
@@ -33,12 +36,28 @@ function LocationPicker() {
       return;
     }
     const location = await getCurrentPositionAsync();
-    console.log(location);
+    setPickedLocation({
+      lat: location.coords.latitude,
+      lng: location.coords.longitude,
+    });
+  }
+  let locationPreview = <Text>No Location Picked yet</Text>;
+
+  if (pickedLocation) {
+    locationPreview = (
+      <Image
+        style={styles.image}
+        source={{
+          uri: getMapPreview(pickedLocation.lat, pickedLocation.lng),
+        }}
+      />
+    );
   }
   function pickOnMapHandler() {}
   return (
     <View>
-      <View style={styles.mapPreview}></View>
+      <View style={styles.mapPreview}>{locationPreview}</View>
+
       <View style={styles.actions}>
         <OutlinedButton icon="location" onPress={getLocationhandler}>
           Locate a User
@@ -65,5 +84,10 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     justifyContent: "space-around",
     alignItems: "center",
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+    borderRadius : 4 
   },
 });
