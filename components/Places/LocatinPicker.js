ffcc4ab0e7249ef0741aad612161135478cf37ad
Abdulrahman Nofal <abdonofa1712@gmail.com>
2023-06-12 13:24:47 +0300

    Changes to be committed: 	modified:   components/Places/LocatinPicker.js 	modified:   util/location.js

--- components/Places/LocatinPicker.js
+++ components/Places/LocatinPicker.js
@@ -7,7 +7,7 @@ import {
   PermissionStatus,
 } from "expo-location";
 import { useEffect, useState } from "react";
-import { getMapPreview } from "../../util/location";
+import { getAddress, getMapPreview } from "../../util/location";
 import {
   useNavigation,
   useRoute,
@@ -35,8 +35,14 @@ function LocationPicker({ onPickLocation }) {
     }
   }, [route, isFocused]);
   useEffect(() => {
-    onPickLocation(pickedLocation);
-  }, [pickedLocation,onPickLocation]);
+    async function handleLocation() {
+      if (pickedLocation) {
+       const address =  await getAddress(pickedLocation.lat, pickedLocation.lng);
+        onPickLocation({...pickedLocation , address:address});
+      }
+    }
+    handleLocation()
+  }, [pickedLocation, onPickLocation]);
 
   async function verifyPermissions() {
     if (
